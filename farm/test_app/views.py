75f4996a03eb959d39ef860f2e3867575a869824
amazing-man <37244101+hagull@users.noreply.github.com>
2018-09-08 16:24:13 +0900

    RequestContext Test / request.user 를 통해 user 객체를 받고 1:N 관계의 테이블에 접근하는것 Test

--- farm/test_app/views.py
+++ farm/test_app/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader, Context
 
 def test_request(request):
+    user = request.user
     return render(request, 'test_app/test_request.html', {
-        'user' : request.user,
-        'test_gcg' : request.user.testgcg_set.all()[0],
+        'user' : user,
+        'test_gcg' : user.testgcg_set.all()[0],
     })
 # Create your views here.
