b4312dad6cf4ca812e5eba1ff72a72bf1f62e5e5
KimYG <dudrjs145@naver.com>
2018-10-11 13:51:41 +0900

    css 정리 및 cctv 영상 송출(이건 무조건 수정 필요)

--- farm/farm/static/js/palm.js
+++ farm/farm/static/js/palm.js
@@ -1,1 +1,17 @@
 
+
+function m_btn(){
+  var c_nav = document.getElementsByClassName('control-nav');
+  var m_nav = document.getElementsByClassName('mobile_nav');
+  var value = "off";
+  if( this.value == 'off') {
+    this.value = 'on';
+    c_nav[0].style.display = 'none';
+    c_nav[0].style.zIndex = '1002';
+  } else {
+    this.value='off';
+    c_nav[0].style.display = 'block';
+    c_nav[0].style.zIndex = '3';
+  }
+
+}
